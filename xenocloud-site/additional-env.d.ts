87821af3ac85831c9a9364721f7bc9a8e1185510
LaTanaDeiGatti <primianocarpinonemichele@gmail.com>
2026-06-06 09:04:51 +0000

    added

--- xenocloud-site/additional-env.d.ts
+++ xenocloud-site/additional-env.d.ts
@@ -2,3 +2,12 @@ declare module "*.css" {
   const content: { [className: string]: string };
   export default content;
 }
+
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_SUPABASE_URL: string;
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    DISCORD_BOT_TOKEN: string;
+    DISCORD_GUILD_ID: string;
+  }
+}
